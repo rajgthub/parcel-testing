562c4885b5d88d2b6cbd89eaab2781d13329e86c
raj <rraju12@gmail.com>
2018-10-20 23:42:57 +0100

    Add initial setup

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Parcel-testing app
+This app is to use parcel to create a simple ReactJS app
+
+## Technologies 
+- ReactJS
+- SASS
---
+++ src/scripts/index.js
@@ -0,0 +1,31 @@
+import "../style/index.scss";
+import React from "react";
+import { render } from "react-dom";
+
+// const App = () => (
+//     <div>
+//         <h1>Welcome to React App!</h1>
+//     </div>
+// )
+class App extends React.Component {
+    state = {
+        isActive: false
+    }
+    handleBtn = () => {
+        this.setState({isActive:!this.state.isActive})
+    }
+  render() {
+    return (
+      <div>
+          <h1>Welcome to React App - class based!</h1>
+          {
+              this.state.isActive ? <h1>I am active!</h1>
+              : <h1>Not active!</h1>
+          }
+        
+        <button onClick={this.handleBtn}>click me!</button>
+      </div>
+    );
+  }
+}
+render(<App />, document.getElementById("appRoot"));
